8feafbf3d60ccbffec2e74d50de63ae64dd589b8
aldimaldi <taktau534@gmail.com>
2023-09-06 20:23:23 +0700

    menambahkan paragraft 2

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>hello word</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid voluptate veniam sequi culpa eligendi officiis, voluptatibus cumque architecto incidunt quis esse iusto excepturi facilis quia natus exercitationem qui distinctio?</p>
+    <p>test</p>
 </body>
 </html>
